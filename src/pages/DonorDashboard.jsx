be72b3ab1f159b75025d884edf77b4907c628fde
daddychill1414 <raniel.go112@gmail.com>
2026-06-06 16:18:43 +0800

    Fix missing AlertCircle import in DonorDashboard

--- src/pages/DonorDashboard.jsx
+++ src/pages/DonorDashboard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { gsap } from 'gsap';
 import {
   Heart, Package, TrendingUp, MapPin, CheckCircle,
-  Clock, ArrowRight, ShieldCheck, Check
+  Clock, ArrowRight, ShieldCheck, Check, AlertCircle
 } from 'lucide-react';
 import { MagneticButton } from '../components/MagneticButton';
 
